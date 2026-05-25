d7bda0d86743c204f26257b3b7e6928efdafa794
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 16:45:56 -0500

    added imports, added scanner, added variable to launch app

--- src/main/java/com/pluralsight/SearchEngine.java
+++ src/main/java/com/pluralsight/SearchEngine.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+//import file writer / reader. buffer. localdate . scanner
+
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
+
+
 public class SearchEngine {
+    //add main
+    public static void main(String[] args) {
+        //name my scanner
+        Scanner myscanner = new Scanner(System.in);
+
+        //launch app
+        logAction("launch");
+    }
 }
