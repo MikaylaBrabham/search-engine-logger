ac8f22b3fcf0e3b039762d039f351c98c291a65f
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 17:03:42 -0500

    added logcatch logic and exception

--- src/main/java/com/pluralsight/SearchEngine.java
+++ src/main/java/com/pluralsight/SearchEngine.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 //import file writer / reader. buffer. localdate . scanner
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -36,4 +38,33 @@ public class SearchEngine {
     }
 
     //create try catch for logaction file creation
+    public static void logAction(String action) {
+
+        //add try
+        try {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt", true));
+
+                //enter date time formatter for given
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+                //add timestamp
+                String timestamp = LocalDateTime.now().format(formatter);
+
+                //add writer
+                writer.write(timestamp + " " + action);
+                writer.newLine();
+
+                //close writer
+                writer.close();
+
+
+                //add catch
+            } catch (IOException e) {
+                System.out.println("Error writing the log file");
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 }
